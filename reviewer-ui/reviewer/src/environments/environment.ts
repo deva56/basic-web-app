4058eedae66d328263ecb111bc057a6db49a933e
deva56 <antonio.deva@hotmail.com>
2023-07-27 01:37:01 +0200

    Updating Angular client backend api url.

--- reviewer-ui/reviewer/src/environments/environment.ts
+++ reviewer-ui/reviewer/src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
     production: true,
-    apiUrl: "http://basic-web-app-api.eu-central-1.elasticbeanstalk.com"
+    apiUrl: "http://basic-web-app-frontend-client.s3-website.eu-central-1.amazonaws.com"
 };
